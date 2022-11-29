034b6bf3372591858eaf8b0a7e0ff72b32753103
NearFire13 <abonneau.benjamin@laposte.net>
2022-11-29 11:51:06 +0100

    Add void in Transmission.

--- Services/Include/Transmission.h
+++ Services/Include/Transmission.h
@@ -5,4 +5,4 @@
 
 void Transmission_Init(USART_TypeDef * USART, MyTimer_Struct_TypeDef Timer, MyGPIO_Struct_TypeDef PortTX, MyGPIO_Struct_TypeDef PortRX);
 void Transmission_Send(USART_TypeDef * USART, char * message, int length);
-signed char Transmission_Receive();
+signed char Transmission_Receive(void);
