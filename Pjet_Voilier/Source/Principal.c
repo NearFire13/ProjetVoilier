f459ec26f51c8a238ac905e5c1028053c028f5e9
Merveilles AGBETI-MESSAN <marcoperson2000@gmail.com>
2022-11-29 11:16:05 +0100

    Integration et Configuration du Systick

--- Pjet_Voilier/Source/Principal.c
+++ Pjet_Voilier/Source/Principal.c
@@ -1,5 +1,6 @@
 #include "Transmission.h"
 #include "BordageAutomatique.h"
+#include "MySysTick.h"
 
 MyGPIO_Struct_TypeDef PB6; // Sortie PMW pour le Plateau
 MyGPIO_Struct_TypeDef PB10; // USART TX
@@ -17,7 +18,7 @@ int a = 0; // Alpha : angle de la girouette
 int teta = 0;
 int ratio = 0;
 
-int compteur = 0; // Envoyer un message apres quelque millisecondes
+int compteur = 0; // Choisir les fonctions a appeler par l'interruption du Systik
 
 // Definition de quelque messages à envoyer à la commande en fonction de la position du voilier
 char * message_debout       = "Le voilier navigue en vent debout       \r\n";
@@ -28,6 +29,72 @@ char * message_grand_largue = "Le voilier navigue en grand largue      \r\n";
 char * message_arriere      = "Le voilier navigue en vent arriere      \r\n";
 
 signed char usart_reveived_value; // Valeur recu depuis la commande
+
+void interactionGirouetteServoMoteur(void){
+	// Recuperation de la valeur de l'angle alpha
+	a = BAUTO_GetAlpha(Timer2.Timer);
+	if((a >= 0 && a <= 45) || (a >= 315 && a < 360)) // Calcul en vent avant
+	{
+		teta = 0;
+	}
+	else if((a > 45 && a < 180)) // Calcul en Babord
+	{
+		teta = (2*a/3)-30;
+	}
+	else // Calcul en Tribord
+	{
+		teta = -(2*a/3)+210;
+	}
+	
+	// Calcul et Mouvement du Servo Moteur
+	ratio = -(50*teta)/90+100;
+	MyTimer_Ratio(Timer3.Timer, 1, ratio);
+}
+
+void interactionUsartPlateau(void){
+	// Prendre la valeur de la dernière donnée recu
+	usart_reveived_value = Transmission_Receive();
+	
+	// Changer le ratio PMW en fonction de la valeur recu de l'USART
+	if(usart_reveived_value < 0){
+		MyGPIO_Set(PC10.GPIO, PC10.GPIO_Pin);
+		MyTimer_Ratio(Timer4.Timer , 1, -usart_reveived_value*10);
+	}
+	else{
+		MyGPIO_Reset(PC10.GPIO, PC10.GPIO_Pin);
+		MyTimer_Ratio(Timer4.Timer , 1, usart_reveived_value*10);
+	}
+}
+
+void interactionPositionVoilierUsart(void){
+	// Envoie des messages sur le positionnement du bateau
+	if((a > 45 && a <= 50) || (a >= 310 && a < 315)){
+		Transmission_Send(USART3, message_pres, 45);
+	}else if((a > 50 && a <= 60) || (a >= 300 && a < 310)){
+		Transmission_Send(USART3, message_plein, 45);
+	}else if((a > 60 && a <= 100) || (a >= 260 && a < 300)){
+		Transmission_Send(USART3, message_travers, 45);
+	}else if((a > 100 && a <= 170) || (a >= 190 && a < 260)){
+		Transmission_Send(USART3, message_grand_largue, 45);
+	}else if((a > 170 && a < 190)){
+		Transmission_Send(USART3, message_arriere, 45);
+	}else{
+		Transmission_Send(USART3, message_debout, 45);
+	}
+}
+
+void Systick_Callback()
+{
+	compteur++;
+	if(compteur == 1){
+		interactionGirouetteServoMoteur();
+		interactionUsartPlateau();
+	}else if(compteur == 2){
+		interactionPositionVoilierUsart();
+	}else{
+		compteur = 0;
+	}
+}
 
 int main(void){
 	// Configuration du Timer 2 pour le Codeur Incremental
@@ -97,61 +164,15 @@ int main(void){
 	BAUTO_Init(Timer2, PA4);
 	BAUTO_WaitForIndex(Timer2.Timer, PA4);
 	
-		// Activation de la PMW par defaut en Vent Arriere
+	// Activation de la PMW par defaut en Vent Arriere
 	MyTimer_PWM(Timer3.Timer, 1);
 	MyTimer_Ratio(Timer3.Timer, 1, 50); // (50/90)*têta+50
 	
+	// Activer le Systik et mettre la periode de comptage sur 100 ms
+	Systick_Init(7200000);
+	Systick_Prio_IT(0, Systick_Callback);
+	
 	while(1)
 	{
-		// Recuperation de la valeur de l'angle alpha
-		a = BAUTO_GetAlpha(Timer2.Timer);
-		if((a >= 0 && a <= 45) || (a >= 315 && a < 360)) // Calcul en vent avant
-		{
-			teta = 0;
-		}
-		else if((a > 45 && a < 180)) // Calcul en Babord
-		{
-			teta = (2*a/3)-30;
-		}
-		else // Calcul en Tribord
-		{
-			teta = -(2*a/3)+210;
-		}
-		
-		// Calcul et Mouvement du Servo Moteur
-		ratio = -(50*teta)/90+100;
-		MyTimer_Ratio(Timer3.Timer, 1, ratio);
-
-		// Prendre la valeur de la dernière donnée recu
-		usart_reveived_value = Transmission_Receive();
-		
-		// Changer le ratio PMW en fonction de la valeur recu de l'USART
-		if(usart_reveived_value < 0){
-			MyGPIO_Set(PC10.GPIO, PC10.GPIO_Pin);
-			MyTimer_Ratio(Timer4.Timer , 1, -usart_reveived_value*10);
-		}
-		else{
-			MyGPIO_Reset(PC10.GPIO, PC10.GPIO_Pin);
-			MyTimer_Ratio(Timer4.Timer , 1, usart_reveived_value*10);
-		}
-		
-		// Envoie des messages sur le positionnement du bateau
-		if(compteur > 10000){
-			if((a > 45 && a <= 50) || (a >= 310 && a < 315)){
-				Transmission_Send(USART3, message_pres, 45);
-			}else if((a > 50 && a <= 60) || (a >= 300 && a < 310)){
-				Transmission_Send(USART3, message_plein, 45);
-			}else if((a > 60 && a <= 100) || (a >= 260 && a < 300)){
-				Transmission_Send(USART3, message_travers, 45);
-			}else if((a > 100 && a <= 170) || (a >= 190 && a < 260)){
-				Transmission_Send(USART3, message_grand_largue, 45);
-			}else if((a > 170 && a < 190)){
-				Transmission_Send(USART3, message_arriere, 45);
-			}else{
-				Transmission_Send(USART3, message_debout, 45);
-			}
-			compteur = 0;
-		}
-		compteur++;
 	}
 }
